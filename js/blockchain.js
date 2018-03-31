e8d9e8d0e3c3c8728fef16cd3ae61f24f19cc7ca
Magnusducatus <magnusducatuslt@gmail.com>
2018-03-31 22:38:50 +0300

    + address

--- js/blockchain.js
+++ js/blockchain.js
@@ -6,7 +6,7 @@ let username = 'test7';
     function history(){
         golos.api.getAccountHistory(username, 100, 100, function(err, result) {
             document.getElementById('table-hash').innerHTML='';
-            result.forEach(value => {let ss = document.getElementById('table-hash').innerHTML= document.getElementById('table-hash').innerHTML + '<br><div class="border border-secondary text-center">АДРЕС: '+'<h3>'+value[1].trx_id+'</h3></div>'});
+            result.forEach(value => {let ss = document.getElementById('table-hash').innerHTML= document.getElementById('table-hash').innerHTML + '<br><div class="border border-secondary text-center">ADDRESS: '+'<h3>'+value[1].trx_id+'</h3></div>'});
             //console.log(err, result);
         });
     }
